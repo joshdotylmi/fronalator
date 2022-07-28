712090a2102409fe2d35472a8e38e35ef099222f
joshdotylmi <josh.doty@lmi.net>
2022-07-28 13:41:33 -0700

    redesign 2

--- InstallGenWireguard.ps1
+++ InstallGenWireguard.ps1
@@ -1,21 +1,27 @@
-$ENV:orgnetworks = "10.0.0.0/24"
-$ENV:wghostname = "66.117.128.224"
+#These networks are to be routed through the wireguard vpn device.
+$ENV:orgnetworks = "10.0.0.0/24,66.117.128.0/19"
+#This is the ip or DNS name of a wireguard server.
+$ENV:wghostname = "1.1.1.1"
+#These are the name servers you want to use for the wireguard client vpn config
 $ENV:orgdnsservers = "1.1.1.1,8.8.8.8"
-#Probably have the below env varibles 
-$ENV:showui = "yes"
-function run-wgservercommand {
 
-    param (
-        [string[]]$terminalcmd
-    )
-    ssh.exe -vv wgkeygen@$ENV:wghostname -i sshkey -p 42042 -o StrictHostKeyChecking=no $terminalcmd
+#This will do a popup wireguard utility so the user can use wireguard for other vpn's however the device tunnel that wireguard installs doesn't show in the UI at all so it's not super useful unless you're using multiple wireguard vpn's.
+$ENV:showui = "yes"
+
+#This is a previous function but is no longer used. 
+#function run-wgservercommand {
+#
+#    param (
+#        [string[]]$terminalcmd
+#    )
+#    ssh.exe wgkeygen@$ENV:wghostname -i .\sshkey -p 42042 -o StrictHostKeyChecking=no $terminalcmd
 
     #start-process -Verbose  -wait -nonewwindow -FilePath "ssh.exe" -ArgumentList "-vv wgkeygen@$ENV:wghostname -i sshkey -p 42042 -o StrictHostKeyChecking=no $terminalcmd" -WorkingDirectory E:\Github\fronalator
-}
+#}
 
-#if (Test-Path "C:\Program Files\WireGuard\wireguard.exe")
-#{Write-Host "Is already installed"}
-#else {
+if (Test-Path "C:\ProgramData\Wireguard\wg_vpn.conf") {
+    Write-Host "Is already installed"}
+else {
     write-host "Attempting key gen"
     #God have mercy on my soul for I'm a foolish girl.
     icacls .\sshkey /inheritance:r
@@ -23,14 +29,19 @@ function run-wgservercommand {
     icacls .\sshkey /grant BUILTIN\Administrators:`(F`)
     #icacls .\sshkey /grant azuread\josh.doty@lmi.net:`(F`)
     #icacls .\sshkey /grant lmi\josh.doty@lmi.net:`(F`)
-    run-wgservercommand $("'"+"./genClient.sh "+$env:COMPUTERNAME+" "+$ENV:orgdnsservers+" "+ $ENV:orgnetworks+"'") 
+     New-Item -Type Directory "C:\ProgramData\Wireguard" -Force
+     $(ssh.exe wgkeygen@$ENV:wghostname -i .\sshkey -p 42042 -o StrictHostKeyChecking=no ./genClient.sh $env:COMPUTERNAME $ENV:orgdnsservers $ENV:orgnetworks)>"C:\ProgramData\Wireguard\wg_vpn.conf"
 
     Get-Process -Name 'WireGuard' | Stop-Process -Force
-    New-Item -Type Directory "C:\ProgramData\Wireguard" -Force
-    Invoke-WebRequest -Uri https://download.wireguard.com/windows-client/wireguard-installer.exe -OutFile wireguard-installer.exe -UseBasicParsing 
+   
+    Invoke-WebRequest -Uri https://download.wireguard.com/windows-client/wireguard-amd64-0.5.3.msi -OutFile wireguard-amd64.msi -UseBasicParsing 
     #From https://web.archive.org/web/20211228190654/https://old.reddit.com/r/WireGuard/comments/rqkxz7/alwayson_vpn/hqb7x3t/
-    Start-Process -Wait -NoNewWindow -FilePath wireguard-installer.exe -ArgumentList   "/qn /installtunnelservice", "C:\ProgramData\Wireguard\wg_vpn.conf"
-    Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\WireGuardTunnelwg_vpn\' -Name 'DelayedAutostart' -Type DWord -Value 1 -PassThru
+    MsiExec /i wireguard-amd64.msi /qn DO_NOT_LAUNCH=1
+
+     timeout 10
+
+   Start-Process "C:\Program Files\WireGuard\wireguard.exe" -ArgumentList "/installtunnelservice C:\ProgramData\Wireguard\wg_vpn.conf" -wait
+    Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Services\WireGuardTunnel$wg_vpn\' -Name 'DelayedAutostart' -Type DWord -Value 1 -PassThru
     if($ENV:showui -like "yes") {
         write-host  "Wireguard UI will be show during login of Windows"
         Set-ItemProperty -Path "HKLM:\SOFTWARE\WireGuard\" -Name "LimitedOperatorUI" -Type DWord -Value 1 -PassThru
@@ -40,10 +51,15 @@ function run-wgservercommand {
      }
     Get-Process -Name 'WireGuard' | Stop-Process -Force
     New-Item -Type Directory "C:\ProgramData\Wireguard" -Force
-    Restart-Service 'WireGuardTunnelwg_vpn' -PassThru | Format-List
+    timeout 10
+    Restart-Service 'WireGuardTunnel$wg_vpn' -PassThru | Format-List
+
+   # Remove-Item .\sshkey -Force
+   remove-item -force ./wireguard-amd64.msi
+   schtasks /create /f /ru SYSTEM /sc daily /tn "WireGuard Update" /tr "%PROGRAMFILES%\WireGuard\wireguard.exe /update" /st 03:00
+
+   wireguard /dumplog |Out-File -Force C:\ProgramData\Wireguard\log.log
 
 
-
-
-#}
+}
 
--- README.md
+++ README.md
@@ -9,6 +9,8 @@ ssh-keygen -t ed25519 -f ~/.ssh/id_ed25519 -N ""
 cat .ssh/id_ed25519.pub > ~/.ssh/authorized_keys
 copy file to hdd putty messes up the formatting somehow.
 cat ~/.ssh/id_ed25519
+copy genClient.sh to home folder for wgkeygen
+make file executable chmod +x ./genClient.sh
 
 sudo sed -i 's/#\?\(Port\s*\).*$/\1 42042/' /etc/ssh/sshd_config
 sudo sed -i 's/#\?\(PubkeyAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
@@ -19,4 +21,4 @@ sudo chown -R :wgkeygen /etc/wireguard
 sudo visudo
 
 add to the end of file
-wgkeygen All = (root) NOPASSWD: /usr/bin/wg addconf wg0 *
+wgkeygen ALL=(ALL) NOPASSWD: /usr/bin/wg syncconf *
